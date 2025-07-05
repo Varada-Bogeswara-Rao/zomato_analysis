e7c4b0d8dc31c9aaa52004342e1478c339688ace
Varada-Bogeswar-Rao <322103310245@gvpce.ac.in>
2025-07-06 00:45:04 +0530

    Cleaning the data

--- Data.py
+++ Data.py
@@ -12,12 +12,20 @@ data = pd.read_csv("D:\Bangalore_Restaurants.csv")
 data1 = pd.read_csv("D:\Pune Restaurants.csv")
 print(data.head(5))
 print(data1.head(5))
-<<<<<<< HEAD
 print(data.shape)
 print(data.columns)
 print(data.isnull().sum())
 print(data1.shape)
 print(data1.columns)
 print(data1.isnull().sum())
-=======
->>>>>>> bc285e57f945e6e5a6013b8415171aba8a9fbe1f
+data1['Delivery_Rating'].fillna(data1['Delivery_Rating'].mean(),inplace =True)
+data1['Known_for1'].fillna(data1['Known_for1'].mode()[0],inplace=True)
+data1['Known_for2'].fillna(data1['Known_for2'].mode()[0],inplace=True)
+data1['Delivery_Rating_Count'].fillna(data1['Delivery_Rating_Count'].mean(),inplace =True)
+print(data1.isnull().sum())
+data['Delivery_Rating'].fillna(data['Delivery_Rating'].mean(),inplace= True)
+data['Dining_Rating'].fillna(data['Dining_Rating'].mean(),inplace=True)
+data['Dining_Review_Count'].fillna(data['Dining_Review_Count'].mean(),inplace =True)
+data['Delivery_Rating_Count'].fillna(data['Delivery_Rating_Count'].mean(),inplace = True)
+print(data.isnull().sum())
+ 
